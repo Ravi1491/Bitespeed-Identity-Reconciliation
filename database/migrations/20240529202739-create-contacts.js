e771d584f8d3beef5633d9ce812dc0a53dee7e4b
Ravi Singh <ravi149185@gmail.com>
2024-05-30 02:00:41 +0530

    feat: Add migration for creating contacts table

---
+++ database/migrations/20240529202739-create-contacts.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('contacts', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      linkedId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'contacts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+      },
+      linkPrecedence: {
+        type: DataTypes.ENUM('primary', 'secondary'),
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('contacts');
+  },
+};
